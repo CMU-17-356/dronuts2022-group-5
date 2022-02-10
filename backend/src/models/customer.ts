1866150de3d683b70d6dfd7c90ef9c890ad09329
Brian Luo <brianluo999@gmail.com>
2022-02-10 15:48:33 -0500

    Update CI

--- backend/src/models/customer.ts
+++ backend/src/models/customer.ts
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-const slug = require('slug');
-const { v4: uuidv4 } = require('uuid');
+// const mongoose = require('mongoose');
+// const uniqueValidator = require('mongoose-unique-validator');
+// const slug = require('slug');
+// const { v4: uuidv4 } = require('uuid');
 
-var customerSchema = new mongoose.Schema({
-    customerId: Number,
-}, {timestamps: true});
+// var customerSchema = new mongoose.Schema({
+//     customerId: Number,
+// }, {timestamps: true});
