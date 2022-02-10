6c5fc37cdabc87d7540c8067feb072625c5f39f3
ZhechunZhou <zhechunzhou@gmail.com>
2022-02-10 17:24:49 -0500

    update app

--- frontend/src/containers/App.tsx
+++ frontend/src/containers/App.tsx
@@ -1,16 +1,16 @@
-
 import '../styles/App.css';
 import {Route, Switch} from 'react-router-dom'
 import {homepage} from "./homepage";
-function App() {
+import * as React from "react";
+
+export const App: React.FC = () => {
   return (
       <Switch>
         <Route path="/" component={homepage}/>
         <Route path="/home"component={homepage}/>
-        <Route path="/projects" component={homepage}/>
-        <Route path="/blogs" component={homepage}/>
+        <Route path="/menu" component={homepage}/>
+        <Route path="/confirm" component={homepage}/>
+        <Route path="/shoppingcart" component={homepage}/>
       </Switch>
   );
 }
-
-export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom";
 import './styles/index.css';
-import App from './containers/App';
+import {App} from "./containers";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
