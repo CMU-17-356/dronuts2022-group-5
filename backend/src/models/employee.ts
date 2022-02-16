060fd5fb126cbf023734788f8a5ba326e8e7ce5a
hengw0498 <78409397+hengw0498@users.noreply.github.com>
2022-02-15 19:50:21 -0500

    Update employee.ts

--- backend/src/models/employee.ts
+++ backend/src/models/employee.ts
@@ -20,10 +20,9 @@ const EmployeeSchema: Schema = new Schema({
 
     EmailAddress: {
         type: String,
-        validate: {
-            validator: () => Promise.resolve(false),
-            message: 'Email validation failed'
-        }
+        validator: function(e:string) {
+            return (new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)).test(e);
+        },
     }
 
 }, {timestamps: true});
