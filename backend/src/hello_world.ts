89cb1a738fe129ab3b362db08b6a467662870f11
ZhechunZhou <zhechunzhou@gmail.com>
2022-02-22 16:02:11 -0500

    save

--- backend/src/hello_world.ts
+++ backend/src/hello_world.ts
@@ -1,10 +1,17 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http';
+import router  from './routes'
+import mongoose from "mongoose";
 require('./models')
 require('./routes')
+
 import express from "express"
 const cors = require('cors')
 const HOST_NAME = '127.0.0.1';
 const PORT = 3000;
+
+mongoose.connect('mongodb://127.0.0.01/db');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
 
 const app = express()
@@ -12,6 +19,4 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
-app.use(require('routes'))
-const server = app.listen( process.env.PORT || PORT, function(){
-});
+app.use(router)
--- backend/src/routes/customer.ts
+++ backend/src/routes/customer.ts
@@ -1,22 +1,33 @@
 import {Router} from "express";
 import mongoose from "mongoose";
+import {CustomerModel} from "../models/customer";
 
 const customerRouter = Router();
 const Donut = mongoose.model('donut');
 const Customer = mongoose.model('customer')
+const Order
 
-customerRouter.get('/menu', function(req, res, next){
-        Donut.find({}, function(err,Donuts){
-            res.send(Donuts)
+customerRouter.get('/profile', function(req, res, next){
+        CustomerModel.find({_id: 1}, function(err,customer){
+            if (err){
+                res.sendStatus(500);
+            }
+            res.send(Customer);
         })
     }
     )
 
 customerRouter.post('/confirm', function(req,res, next){
-    Customer.findById(req.body.id)
+    const customer = Customer.findById(req.body.id)
+
+
+
+
 })
 
 customerRouter.post('/order', function(req, res, next){
+    Customer.findById(req.body.id)
+    //TODO
 
 })
 
