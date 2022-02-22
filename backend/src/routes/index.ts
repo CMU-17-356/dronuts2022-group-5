850342d5c5bcabe98ff970572ce407967a1e5618
ZhechunZhou <zhechunzhou@gmail.com>
2022-02-22 16:22:01 -0500

    Reformat routing

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import {Router} from "express";
 import customerRouter from "./customer"
 import donutRouter from "./donut";
+import {employeeRouter} from "./employee"
 
 const router = Router()
 router.use('/customer', customerRouter)
 router.use('/donut', donutRouter)
+router.use('/employee', employeeRouter)
 
 export default router;
