a9f686cc68649a37bb9ecfd9af2762da4fbbb64a
Adam Wang <adamwang@andrew.cmu.edu>
2022-02-23 00:24:54 -0500

    Status page

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {Menu} from "./Menu";
 import {HomePage} from "./HomePage";
+import {Status} from "./Status";
 import * as React from "react";
 
 export const App: React.FC = () => {
@@ -11,6 +12,7 @@ export const App: React.FC = () => {
         <Route path="/home" element={<HomePage />}/>
         <Route path="/menu" element={<Menu />}/>
         <Route path="/confirm" element={<HomePage />}/>
+        <Route path="/status" element={<Status />}/>
         <Route path="/shoppingcart" element={<HomePage />}/>
       </Routes>
     </BrowserRouter>
