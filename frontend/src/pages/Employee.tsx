1728877fcb017ddd666c02ec580319726e7ca513
Brian Luo <brianluo999@gmail.com>
2022-02-27 18:53:54 -0500

    Use db data for employee page

--- frontend/src/pages/Employee.tsx
+++ frontend/src/pages/Employee.tsx
@@ -2,77 +2,124 @@ import * as React from "react";
 import "../styles/Employee.css";
 import {DonutCard} from "../components/DonutCard";
 import "../styles/Menu.css";
+import { getRequest, postRequest } from "../utils/requests";
+import { OrderButton } from "../components/OrderButton";
+import { OrderInterface } from "../types/api";
 
 class Donut {
+    id: string
     name: string
-    image: string
-    description: string
     price: number
-    quantity: number
-
-    constructor(name: string, image: string, description: string, price: number, quantity: number) {
-        this.name = name;
-        this.image = image;
-        this.description = description;
-        this.price = price;
-        this.quantity = quantity;
-    }
+    weight: number
+    discount: number
+    picture: string
+    description: string
+    availability: boolean
+    tags: Array<string>
 }
 
-// Temporary Menu
-const items: Array<Donut> = [
-    new Donut("Apple Krumble", "https://cmu-17-356.github.io/Dronuts/assets/donut_flavors/apple_krumb.jpg", "Apples and crumbs", 3.99, 0),
-    new Donut("Apple Krumble", "https://cmu-17-356.github.io/Dronuts/assets/donut_flavors/apple_krumb.jpg", "Apples and crumbs", 3.99, 2),
-    new Donut("Apple Krumble", "https://cmu-17-356.github.io/Dronuts/assets/donut_flavors/apple_krumb.jpg", "Apples and crumbs", 3.99, 0),
-    new Donut("Apple Krumble", "https://cmu-17-356.github.io/Dronuts/assets/donut_flavors/apple_krumb.jpg", "Apples and crumbs", 3.99, 0),
-    new Donut("Apple Krumble", "https://cmu-17-356.github.io/Dronuts/assets/donut_flavors/apple_krumb.jpg", "Apples and crumbs", 3.99, 0),
-    new Donut("Apple Krumble", "https://cmu-17-356.github.io/Dronuts/assets/donut_flavors/apple_krumb.jpg", "Apples and crumbs", 3.99, 0),
-    new Donut("Apple Krumble", "https://cmu-17-356.github.io/Dronuts/assets/donut_flavors/apple_krumb.jpg", "Apples and crumbs", 3.99, 0),
-]
+export class Order {
+    id: string
+    amounts: Array<number>
+    status: string
+    tax: number
+    serviceFee: number
+    deliveryFee: number
+    totalCost: number
+    rating: number
+    customer: string
+    donuts: Array<string>
+}
 
 export const Employee: React.FC = () => {
+    // TODO: add confirmOrder to order buttons
+    async function confirmOrder(order: Order) {
+        alert("confirm order");
+        try {
+            const res = await postRequest<OrderInterface>(order, `employee/confirm?orderId=${order.id}`, null);
+            if (res.status == 200) {
+                console.log("success");
+            } else {
+                console.log(res);
+            }
+            // Refresh?
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const [menu, setMenu] = React.useState<Array<Donut>>([]);
+    async function getMenu() {
+        try {
+            const res = await getRequest<Array<Donut>>("donut/donuts", {});
+            if (res.status === 200) {
+                setMenu(res.data);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    React.useEffect(() => {
+            getMenu();
+    }, []);
+
+    const menuDict: { [key: string] : Donut } = {};
+    menu.forEach(donut => {
+        menuDict[donut.id] = donut;
+    });
+
+    const [orders, setOrders] = React.useState<Array<Order>>([]);
+    async function getOrders() {
+        try {
+            const res = await getRequest<Array<Order>>("employee/orders", {});
+            if (res.status == 200) {
+                setOrders(res.data);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    React.useEffect(() => {
+        getOrders();
+    }, []);
+
+    let donuts: Array<string> = [];
+    let amounts: Array<number> = [];
+    if (orders.length > 0) {
+        donuts = orders[0].donuts;
+        amounts = orders[0].amounts;
+    }
+
     return (<>
         <div className="employee-Main-Div">
             <div className="list-orders-div">
                 <h1 className = "employee-main-div-title">Orders</h1>
-                <div className="list-order-button">
-                    <button className = "xButton">x</button>
-                    <button className = "ListButton">Order #3000</button>
-                    <button className = "xButton">x</button>
-                </div>
-                <div className="list-order-button">
-                    <button className = "xButton">x</button>
-                    <button className = "ListButton">Order #3000</button>
-                    <button className = "xButton">x</button>
-                </div>
-                <div className="list-order-button">
-                    <button className = "xButton">x</button>
-                    <button className = "ListButton">Order #3000</button>
-                    <button className = "xButton">x</button>
-                </div>
-                <div className="list-order-button">
-                    <button className = "xButton">x</button>
-                    <button className = "ListButton">Order #3000</button>
-                    <button className = "xButton">x</button>
-                </div>
+                {orders.map((order: Order, index: number) => (
+                    <OrderButton key={index} id={order.id}/>
+                ))}
             </div>
 
             <div className="order-div">
-                <h1 className="order-Title">Order #3000</h1>
+                <h1 className="order-Title">Order</h1>
                 <div className="In-Progress-Div">
                     <h1 className = "In-Progress-Title">In Progress</h1>
                     <div className="In-Progress">
-                        {items.map((donut: Donut, index: number) => (
-                            <DonutCard key={index} name={donut.name} image={donut.image} description={donut.description} price={donut.price} quantity={donut.quantity}/>
-                        ))}
+                        {donuts.map((donutId: string, index: number) => {
+                            const donut = menuDict[donutId];
+                            return <DonutCard
+                                key={index}
+                                name={donut.name}
+                                image={donut.picture}
+                                description={donut.description}
+                                price={donut.price}
+                                quantity={amounts[index]}
+                            />;
+                        })}
                     </div>
                 </div>
                 <div className="Completed-Div">
                     <h1 className = "Completed-Title">Completed</h1>
                     <div className="Completed">
-                        {items.map((donut: Donut, index: number) => (
-                            <DonutCard key={index} name={donut.name} image={donut.image} description={donut.description} price={donut.price} quantity={donut.quantity}/>
-                        ))}
                     </div>
                 </div>
             </div>
