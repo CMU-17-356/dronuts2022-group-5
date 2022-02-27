52c5601051aba710a60af38e8ce6d5aa2e877c5a
Jack Wang <hengw@andrew.cmu.edu>
2022-02-27 17:17:34 -0500

    Adding function: Navigating to Employee view

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -55,6 +55,18 @@ export const NavBar: React.FC = () => {
                                 <span className="ml-2">Account</span>
                             </Link>
                         </li>
+                        <li className={"nav-item items-center"}>
+                            <Link
+                                className={
+                                    "text-gray-800 hover:text-gray-600 px-3 py-0 flex items-center text-center text-sm uppercase font-bold"}
+                                to="/employee">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
+                                </svg>
+                                <span className="ml-2">Employee</span>
+                            </Link>
+                        </li>
+                        
                     </ul>
                 </div>
             </div>
